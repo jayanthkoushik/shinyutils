f391b1e6a9bc609e4aaeb08d3662ef1bd1e0cb90
Jayanth Koushik <jnkoushik@gmail.com>
2019-03-04 19:27:00 -0500

    Simplify logging

--- shinyutils/logng.py
+++ shinyutils/logng.py
@@ -1,21 +1,20 @@
 """logng.py: utilities for logging."""
 
 import logging
+import sys
 
 import crayons
 
 
 def build_log_argp(base_parser):
-    """Build a parser group for logging arguments."""
-    log_parser = base_parser.add_argument_group("logging")
-    log_parser.add_argument(
+    """Add an argument for logging to the base_parser."""
+    base_parser.add_argument(
         "--log-level",
         type=str,
         choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
         default="INFO",
     )
-    log_parser.add_argument("--log-file", type=str, default=None)
-    return log_parser
+    return base_parser
 
 
 class ColorfulLogRecord(logging.LogRecord):
@@ -23,6 +22,7 @@ class ColorfulLogRecord(logging.LogRecord):
     """LogRecord with colors."""
 
     def __init__(self, *args, **kwargs):
+        # pylint: disable=no-member
         super().__init__(*args, **kwargs)
         if self.levelno == logging.CRITICAL:
             colf = crayons.red
@@ -34,7 +34,7 @@ class ColorfulLogRecord(logging.LogRecord):
             colf = crayons.cyan
         else:
             colf = crayons.green
-        self.levelname = str(colf(self.levelname, bold=True))
+        self.levelname = str(colf(self.levelname, bold=True, always=True))
 
         self.msg = (
             crayons.colorama.Style.BRIGHT
@@ -43,7 +43,7 @@ class ColorfulLogRecord(logging.LogRecord):
         )
 
 
-def conf_logging(args=None, log_level=None, log_file=None):
+def conf_logging(args=None, log_level=None):
     """Configure logging using args from `build_log_argp`."""
     if log_level is None:
         if args is not None and hasattr(args, "log_level"):
@@ -52,13 +52,9 @@ def conf_logging(args=None, log_level=None, log_file=None):
             log_level = "INFO"
     log_level_i = getattr(logging, log_level, logging.INFO)
 
-    if log_file is None and args is not None and hasattr(args, "log_file"):
-        log_file = args.log_file
-
     logging.basicConfig(
-        filename=log_file,
         level=log_level_i,
         format="%(levelname)s:%(filename)s.%(funcName)s.%(lineno)d:%(message)s",
     )
-    if log_file is None:
+    if sys.stderr.isatty():
         logging.setLogRecordFactory(ColorfulLogRecord)
