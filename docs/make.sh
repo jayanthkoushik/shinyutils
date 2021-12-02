671ce7c8d2c7734453d98f0383d1a1815c9eeb30
Jayanth Koushik <jnkoushik@gmail.com>
2021-12-01 21:38:38 -0500

    docs: generate separate doc files for each module

--- docs/make.sh
+++ docs/make.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env sh
+# Usage: docs/make.sh  # must be executed from the project root
+# Generates markdown documentation for each top level package, except `tests`.
+# One file is generated per module, which is put in `docs/`.
 
-SPHINX_APIDOC_OPTIONS='members' poetry run sphinx-apidoc -o docs/_build -M -T shinyutils
-mv docs/_build/shinyutils.rst docs/_build/index.rst
-poetry run sphinx-build -D highlight_language=python -b markdown -c docs -d docs/_build/.doctrees docs/_build docs
-sed -i '' 's/ *$//' docs/index.md
+SPHINX_APIDOC_OPTIONS='members' \
+poetry run sphinx-apidoc \
+    -o docs/_build \
+    --module-first \
+    --tocfile index \
+    --separate \
+    . tests
+
+poetry run sphinx-build \
+    -C \
+    -D extensions=sphinx.ext.autodoc,sphinx.ext.napoleon \
+    -D default_role=samp \
+    -D autodoc_member_order=bysource \
+    -D autodoc_typehints=none \
+    -D highlight_language=python \
+    -b markdown \
+    -c docs \
+    -d docs/_build/.doctrees \
+    docs/_build docs `ls docs/_build/*.rst`
+
+find docs -name '*.md' -exec sed -i '' 's/ *$//' {} +
